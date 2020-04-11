fef620bbe07a05382b18c175c751d4116b876f9d
reiven2512 <63257300+reiven2512@users.noreply.github.com>
2020-04-11 09:52:34 +0300

    Update ShowActivity.java

--- version 1/main/java/com/example/laba_3_2/ShowActivity.java
+++ version 1/main/java/com/example/laba_3_2/ShowActivity.java
@@ -30,42 +30,23 @@ public class ShowActivity extends AppCompatActivity {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor c = db.query(TABLE,null, null, null, null, null, null);
         c.moveToFirst();
-        if(db.getVersion() == 1)
-        {
-            add_row(ID, FIO, DATE, null, null);
-            do {
-                String[] s = c.getColumnNames();
-                int id = c.getInt(c.getColumnIndex(s[0]));
-                String fio = c.getString(c.getColumnIndex(s[1]));
-                String date = c.getString(c.getColumnIndex(s[2]));
-                add_row(Integer.toString(id), fio, date, null, null);
-            } while (c.moveToNext());
-        }
-        else {
-            add_row(ID, LAST, FIRST, MIDDLE, DATE);
-            do {
-                String[] s = c.getColumnNames();
-                int id = c.getInt(c.getColumnIndex(s[0]));
-                String last = c.getString(c.getColumnIndex(s[1]));
-                String first = c.getString(c.getColumnIndex(s[2]));
-                String middle = c.getString(c.getColumnIndex(s[3]));
-                String date = c.getString(c.getColumnIndex(s[4]));
-                add_row(Integer.toString(id), last, first, middle, date);
-            } while (c.moveToNext());
-        }
+        add_row(ID, FIO, DATE);
+        do {
+            String[] s = c.getColumnNames();
+            int id = c.getInt(c.getColumnIndex(s[0]));
+            String fio = c.getString(c.getColumnIndex(s[1]));
+            String date = c.getString(c.getColumnIndex(s[2]));
+            add_row(Integer.toString(id), fio, date);
+        } while (c.moveToNext());
     }
-    public void add_row(String id, String last, String first, String middle, String time){
+    public void add_row(String id, String fio, String time){
         TableLayout tl = (TableLayout) findViewById(R.id.layout);
         LayoutInflater inflater = LayoutInflater.from(this);
         TableRow tr = (TableRow) inflater.inflate(R.layout.row, null);
         TextView tv = (TextView) tr.findViewById(R.id.name_1);
         tv.setText(id);
         tv = (TextView) tr.findViewById(R.id.name_2);
-        tv.setText(last);
-        tv = (TextView) tr.findViewById(R.id.name_3);
-        tv.setText(first);
-        tv = (TextView) tr.findViewById(R.id.name_4);
-        tv.setText(middle);
+        tv.setText(fio);
         tv = (TextView) tr.findViewById(R.id.name_5);
         tv.setText(time);
         tl.addView(tr);
